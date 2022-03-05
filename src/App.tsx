85e0c42f98239d3afc13a32ce2e95668f0545a8f
mrmusselman1 <info@matthewrmusselman.com>
2022-03-05 18:34:23 -0500

    moved the countdown into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -5,59 +5,34 @@ import IndexHeader from 'components/Headers/IndexHeader';
 import WeddingCard from 'WeddingCard';
 import DefaultFooter from 'components/Footers/DefaultFooter';
 import Photos from 'Photos';
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import Countdown from 'Countdown';
+
+/*
+ * @todo TODO:
+ * - for our friends far away, we plan to come visit you soon
+ *
+ */
 
 function App() {
 
     const date = 'May 21, 2022';
     const time = '1:00 PM';
 
-    const calculateTimeLeft = () => {
-        let difference = +new Date(`${date} ${time}`) - +new Date();
-        if (difference <= 0) {
-            return "Today's the Day!"
-        }
-
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((difference / 1000 / 60) % 60);
-        const seconds = Math.floor((difference / 1000) % 60);
-
-        let timeString = '';
-        if (days > 0) {
-            let dayLabel = days !== 0 ? 'days' : 'day';
-            timeString += `${days} ${dayLabel} `;
-        }
-        if (hours > 0) {
-            let hourLabel = hours !== 0 ? 'hours' : 'hour';
-            timeString += `${hours} ${hourLabel} `;
-        }
-        if (minutes > 0) {
-            let minuteLabel = minutes !== 0 ? 'minutes' : 'minute';
-            timeString += `${minutes} ${minuteLabel} `;
-        }
-        if (seconds > 0) {
-            let secondsLabel = seconds !== 0 ? 'seconds' : 'second';
-            timeString += `${seconds} ${secondsLabel}`;
-        }
-
-        return timeString;
-    }
-
-    const [timeLeft, setTimeLeft] = useState<string>(calculateTimeLeft());
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
-
-        return () => clearTimeout(timer);
-    });
-
     return (
         <div className="wrapper">
+                {
+                    /*
+                     * Main Image Header, Countdown
+                     */
+                }
             <IndexHeader
                 date={date}
-                countdown={timeLeft}
+                countdown={
+                    <Countdown
+                        date={date}
+                        time={time}
+                    />
+                }
             />
             <Container>
                 {
---
+++ src/Countdown.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+interface ICountdownProps {
+    date: string,
+    time: string,
+}
+
+const Countdown: React.FC<ICountdownProps> = ({
+    date, time,
+}) => 
+{
+    const calculateTimeLeft = () => {
+        let difference = +new Date(`${date} ${time}`) - +new Date();
+        if (difference <= 0) {
+            return "Today's the Day!"
+        }
+
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((difference / 1000 / 60) % 60);
+        const seconds = Math.floor((difference / 1000) % 60);
+
+        let timeString = '';
+        if (days > 0) {
+            let dayLabel = days !== 0 ? 'days' : 'day';
+            timeString += `${days} ${dayLabel} `;
+        }
+        if (hours > 0) {
+            let hourLabel = hours !== 0 ? 'hours' : 'hour';
+            timeString += `${hours} ${hourLabel} `;
+        }
+        if (minutes > 0) {
+            let minuteLabel = minutes !== 0 ? 'minutes' : 'minute';
+            timeString += `${minutes} ${minuteLabel} `;
+        }
+        if (seconds > 0) {
+            let secondsLabel = seconds !== 0 ? 'seconds' : 'second';
+            timeString += `${seconds} ${secondsLabel}`;
+        }
+
+        return timeString;
+    }
+
+    const [timeLeft, setTimeLeft] = useState<string>(calculateTimeLeft());
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
+
+    return (
+        <div title={`${date} ${time} Eastern Standard Time`}>
+            <p>{timeLeft}</p>
+        </div>
+    );
+};
+
+export default Countdown;
